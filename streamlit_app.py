069d8c3317a22e6c8b2433ede01dc58476b3fc14
eklesia-lee <eklesia7@snu.ac.kr>
2022-11-15 15:48:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 """
 # Welcome to DEmoDemo
 """
-write('Hello, *World!* :sunglasses:')
+st.write('Hello, *World!* :sunglasses:')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
