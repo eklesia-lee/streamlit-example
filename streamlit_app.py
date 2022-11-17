1342f84e85d20f19e45d618353c285c93b7cd6d5
eklesia-lee <eklesia7@snu.ac.kr>
2022-11-18 06:48:08 +0900

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import streamlit_authenticator as stauth
+import streamlit-authenticator as stauth
 
 
 name, authentication_status, username = authenticator.login('Login', 'main')
