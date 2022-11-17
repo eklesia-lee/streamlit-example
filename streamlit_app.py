35238445c19e9cba72dfdea52ebf9b80bf23491c
eklesia-lee <eklesia7@snu.ac.kr>
2022-11-18 06:55:18 +0900

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,18 @@ import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
 
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
 name, authentication_status, username = authenticator.login('Login', 'main')
 
 """
